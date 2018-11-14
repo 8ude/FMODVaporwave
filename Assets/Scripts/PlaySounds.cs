8da790365f26d1edbc770303e995ffbb688f660b
8ude <crbertelsen@gmail.com>
2018-11-14 14:04:30 -0500

    Adding Unity and FMOD Project

---
+++ Assets/Scripts/PlaySounds.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+public class PlaySounds : MonoBehaviour {
+
+	[FMODUnity.EventRef] public string playVHSSound;
+
+	[FMODUnity.EventRef] public string playLongerVHSSound;
+
+	[FMODUnity.EventRef] public string televisionLoop;
+
+	public string TelevisionOffParam;
+	
+	private FMOD.Studio.EventInstance televisionLoopInstance;
+
+	private bool playingTVSound;
+	
+	
+	// Use this for initialization
+	void Start () {
+
+		televisionLoopInstance = FMODUnity.RuntimeManager.CreateInstance(televisionLoop);
+		playingTVSound = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.C)) {
+			//when I press C, I want to play the short VHS Sound wherever this game object is
+			FMODUnity.RuntimeManager.PlayOneShot(playVHSSound, transform.position);
+		}
+
+		if (Input.GetKeyDown(KeyCode.M)) {
+			//when I press M, I want to play the longer VHS sound, wherever this game object is
+			FMODUnity.RuntimeManager.PlayOneShotAttached(playLongerVHSSound, gameObject);
+		}
+
+		if (Input.GetKey(KeyCode.Y)) {
+			//when I first press Y, we want to start the television loop
+			//as I keep holding Y down, we want the loop to continue
+
+			if (!playingTVSound) {
+				televisionLoopInstance.setParameterValue("TelevisionOff", 0.0f);
+				televisionLoopInstance.start();
+				playingTVSound = true;
+			}
+			
+		} else if (Input.GetKeyUp(KeyCode.Y)) {
+			//when I release Y, we want the loop to exit, and set playingTVSound back to false
+			televisionLoopInstance.setParameterValue("TelevisionOff", 1.0f);
+			playingTVSound = false;
+		}
+		
+	}
+}
---
+++ Assets/Scripts/SoundsOnCommand.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundsOnCommand : MonoBehaviour {
+	
+	//These first two are the easiest way to make sound in FMOD via scripting - we
+	//do a "play one shot" that plays the VHS Event
+	[FMODUnity.EventRef]
+	public string VHSAudioEvent;
+
+	[FMODUnity.EventRef] 
+	public string LongerVHSAudioEvent;
+	
+	//This one is a bit different - we need 
+	[FMODUnity.EventRef] 
+	public string TVSustainEvent;
+	private FMOD.Studio.EventInstance tvEvent;
+
+	private bool turnedOnTV = false;
+	
+	
+	
+	
+	//we're going to have an arbitrary parameter that we increase by pressing 'C',
+	//and use this to adjust things in FMOD
+	public float playerCoolness = 0f;
+	
+	// Use this for initialization
+	void Start () {
+		//We need to create an instance of our TV event, even before we start playing it.	
+		tvEvent = FMODUnity.RuntimeManager.CreateInstance(TVSustainEvent);
+		turnedOnTV = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(KeyCode.C)) {
+			PlayOneShotSound();
+		}
+		else if (Input.GetKeyDown(KeyCode.M)) {
+			PlayTrackedOneShotSound();
+		}
+		
+		
+		//This Controls the Television Loop Sound
+
+		if (Input.GetKey(KeyCode.Y)) {
+			//manage sustained sound
+			if (!turnedOnTV) {
+				tvEvent.setParameterValue("TelevisionOff", 0f);
+				turnedOnTV = true;
+				Debug.Log("Turned On TV");
+				tvEvent.start();
+			}
+			
+		} else if (Input.GetKeyUp(KeyCode.Y)) {
+			//when i release the key, i turn off the tv
+			tvEvent.setParameterValue("TelevisionOff", 1.0f);
+			turnedOnTV = false;
+		}
+
+		if (Input.GetKeyDown(KeyCode.K)) {
+			playerCoolness++;
+			//Attach to FMOD Parameter
+		}
+		
+		//todo --
+		//have the following functions working for the demo on Monday:
+		//pressing C does the vhs sound as a one shot (unattached)
+		//pressing M does the vhs sound as a one shot (attached)
+		//pressing and holding Y goes through an AHDSR - esque curve
+		//pressing K increases coolness, which adjusts a global audio effect
+		
+	}
+
+	void PlayOneShotSound() {
+		//PlayOneShot functions similarly as it does in unity, though now we can give it a position
+		//This is probably the easiest way to 
+		FMODUnity.RuntimeManager.PlayOneShot(VHSAudioEvent, gameObject.transform.position);
+	}
+
+	void PlayTrackedOneShotSound() {
+		//This will keep track of the game object's position over the sound's lifetime
+		FMODUnity.RuntimeManager.PlayOneShotAttached(LongerVHSAudioEvent, gameObject);
+	}
+}
